3af7130bf01ce4996ccfa81888e4fe6d0ad331ab
Owen Kujo <owenrapeepat@gmail.com>
2025-06-21 13:47:30 +0700

    test3

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,11 @@
 module.exports = {
-    name: 'ping',
-    description: 'Replies with Pong!',
-    
-    callback: (client, interaction) => {
+    // The data object is required by djs-commander for slash commands
+    data: {
+        name: 'ping',
+        description: 'Replies with Pong! to check if the bot is running.',
+    },
+    // The run function is executed when the command is used
+    run: ({ interaction, client, handler }) => {
         interaction.reply('Pong!');
     },
 };
